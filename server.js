9c061f3d6e6cf9eea6bc8ebf7bfd8f006b358ca5
kishanti <109414041+kishanti@users.noreply.github.com>
2024-03-08 18:27:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use("/public", express.static(path.join(__dirname, "./public")));
+
+app.use("/api", require("./src/routes/index"));
+
+app.listen(port, (err, result) => {
+  console.log(`Server Running On ${port}`);
+});
